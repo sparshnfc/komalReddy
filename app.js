15fc21ac438a80133725ee6ceed2a1d875790e33
Saikiran Rudra <Saikiranrudra31@gmail.com>
2025-08-19 13:31:48 +0530

    trial

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import ejs from "ejs-mate"
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("<h1>Hello</h1>")
+})
+
+app.get("/site", (req,res)=>{
+    res.render("index.ejs");
+})
+
+app.listen(3000, (req,res)=>{
+    console.log(`Port start workingon ${3000}`);  
+})
